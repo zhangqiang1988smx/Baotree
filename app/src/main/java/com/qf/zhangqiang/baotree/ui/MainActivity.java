2a0e1ca8f3da4cf84c21951966b6d02fdb520228
smxqiang <57003623@qq.com>
2016-01-11 10:33:50 +0800

    version 1.1

--- app/src/main/java/com/qf/zhangqiang/baotree/ui/MainActivity.java
+++ app/src/main/java/com/qf/zhangqiang/baotree/ui/MainActivity.java
@@ -1,9 +1,10 @@
-package com.qf.zhangqiang.baotree;
+package com.qf.zhangqiang.baotree.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.qf.zhangqiang.baotree.R;
+
+public class MainActivity  extends BaseActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
